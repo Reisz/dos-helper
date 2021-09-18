9aa18f13b67e8893fa94729a7e956197d8ede9b7
Reisz <Reisz@users.noreply.github.com>
2021-09-18 16:36:20 +0200

    Add all materials and potions

--- data-processor/src/crafting.rs
+++ data-processor/src/crafting.rs
@@ -25,7 +25,7 @@ impl Item {
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(untagged)]
 enum Ingredient {
     Unique(String),
@@ -48,24 +48,115 @@ impl Ingredient {
 type Recipes = Vec<Recipe>;
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Recipe {
-    input: Vec<Ingredient>,
-    output: Vec<String>,
+#[serde(untagged)]
+enum Recipe {
+    Regular {
+        input: Vec<Ingredient>,
+        output: Vec<String>,
+    },
+    PotionUpgrade {
+        potion_levels: Vec<String>,
+        hq_augmentors: Option<u8>,
+        ult_augmentors: Option<u8>,
+    },
 }
 
+const AUGMENTORS: &[&str] = &[
+    "Augmentor",
+    "High Quality Augmentor",
+    "Ultimate Augmentor Herb",
+];
+
 impl Recipe {
-    fn convert(self, item_map: &ItemMap) -> output::Recipe {
+    fn build_recipe(
+        input: Vec<Ingredient>,
+        output: Vec<String>,
+        item_map: &ItemMap,
+    ) -> output::Recipe {
         let mut result = output::Recipe::new();
 
-        for input in self.input {
+        for input in input {
             result.add_input(input.convert(item_map));
         }
 
-        for output in self.output {
+        for output in output {
             result.add_output(*item_map.get(&output).unwrap())
         }
 
         result
+    }
+
+    fn add_potion_recipes(
+        recipes: &mut Vec<output::Recipe>,
+        input: String,
+        output: String,
+        augmentor_count: u8,
+        item_map: &ItemMap,
+    ) {
+        let augmentors = AUGMENTORS
+            .iter()
+            .cloned()
+            .skip(AUGMENTORS.len() - augmentor_count as usize)
+            .map(str::to_owned)
+            .collect();
+
+        recipes.push(Self::build_recipe(
+            vec![Ingredient::Unique(input.clone()); 2],
+            vec![output.clone()],
+            item_map,
+        ));
+
+        recipes.push(Self::build_recipe(
+            vec![
+                Ingredient::Unique(input.clone()),
+                Ingredient::Alternatives(augmentors),
+            ],
+            vec![output.clone()],
+            item_map,
+        ));
+    }
+
+    fn convert(self, item_map: &ItemMap) -> Vec<output::Recipe> {
+        match self {
+            Self::Regular { input, output } => {
+                vec![Self::build_recipe(input, output, item_map)]
+            }
+            Self::PotionUpgrade {
+                mut potion_levels,
+                hq_augmentors,
+                ult_augmentors,
+            } => {
+                let mut recipes = Vec::new();
+
+                let mut hq_augmentors = hq_augmentors.unwrap_or(1);
+                let mut ult_augmentors = ult_augmentors.unwrap_or(1);
+
+                potion_levels.reverse();
+                for (input, output) in potion_levels.iter().skip(1).zip(potion_levels.iter()) {
+                    let (input, output) = (input.to_owned(), output.to_owned());
+
+                    let augmentor_count = if ult_augmentors > 0 {
+                        ult_augmentors -= 1;
+                        1
+                    } else if hq_augmentors > 0 {
+                        hq_augmentors -= 1;
+                        2
+                    } else {
+                        3
+                    };
+
+                    Self::add_potion_recipes(
+                        &mut recipes,
+                        input,
+                        output,
+                        augmentor_count,
+                        item_map,
+                    );
+                }
+
+                recipes
+            }
+        }
     }
 }
 
@@ -93,7 +184,9 @@ pub fn process() {
     }
 
     for recipe in recipes {
-        crafting.add_recipe(recipe.convert(&item_map))
+        for recipe in recipe.convert(&item_map) {
+            crafting.add_recipe(recipe);
+        }
     }
 
     let file = File::create("dist/data.bin.xz").unwrap();
